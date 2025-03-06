42c5839e40eb63d3bd6ad0a8ff524f9d1fb9aaa7
Chenyang Yu <asuradayuci@gmail.com>
2025-03-06 18:02:31 +0800

    Add files via upload

---
+++ climb/loss.py
@@ -0,0 +1,270 @@
+import collections
+from abc import ABC
+
+import numpy as np
+import torch
+import torch.nn.functional as F
+from torch import autograd, nn
+from torch.cuda import amp
+
+
+class CM(autograd.Function):
+
+    @staticmethod
+    @amp.custom_fwd
+    def forward(ctx, inputs, targets, features, momentum):
+        ctx.features = features
+        ctx.momentum = momentum
+        ctx.save_for_backward(inputs, targets)
+        outputs = inputs.mm(ctx.features.t())
+
+        return outputs
+
+    @staticmethod
+    @amp.custom_bwd
+    def backward(ctx, grad_outputs):
+        inputs, targets = ctx.saved_tensors
+        grad_inputs = None
+        if ctx.needs_input_grad[0]:
+            grad_inputs = grad_outputs.mm(ctx.features)
+
+        # momentum update
+        for x, y in zip(inputs, targets):
+            ctx.features[y] = ctx.momentum * ctx.features[y] + (1. - ctx.momentum) * x
+            ctx.features[y] /= ctx.features[y].norm()
+
+        return grad_inputs, None, None, None
+
+
+
+
+class CM_Hard(autograd.Function):
+
+    @staticmethod
+    @amp.custom_fwd
+    def forward(ctx, inputs, targets, features, momentum):
+        ctx.features = features
+        ctx.momentum = momentum
+        ctx.save_for_backward(inputs, targets)
+        outputs = inputs.mm(ctx.features.t())
+
+        return outputs
+
+    @staticmethod
+    @amp.custom_bwd
+    def backward(ctx, grad_outputs):
+        inputs, targets = ctx.saved_tensors
+        grad_inputs = None
+        if ctx.needs_input_grad[0]:
+            grad_inputs = grad_outputs.mm(ctx.features)
+
+        batch_centers = collections.defaultdict(list)
+        for instance_feature, index in zip(inputs, targets.tolist()):
+            batch_centers[index].append(instance_feature)
+
+        for index, features in batch_centers.items():
+            distances = []
+            for feature in features:
+                distance = feature.unsqueeze(0).mm(ctx.features[index].unsqueeze(0).t())[0][0]
+                distances.append(distance.cpu().numpy())
+
+            median = np.argmin(np.array(distances))
+            ctx.features[index] = ctx.features[index] * ctx.momentum + (1 - ctx.momentum) * features[median]
+            ctx.features[index] /= ctx.features[index].norm()
+
+        return grad_inputs, None, None, None
+
+def cm(inputs, indexes, features, momentum=0.5):
+    return CM.apply(inputs, indexes, features, torch.Tensor([momentum]).to(inputs.device))
+
+def cm_hard(inputs, indexes, features, momentum=0.5):
+    return CM_Hard.apply(inputs, indexes, features, torch.Tensor([momentum]).to(inputs.device))
+
+
+class ClusterMemoryAMP(nn.Module, ABC):
+    def __init__(self, temp=0.05, momentum=0.2, use_hard=False):
+        super(ClusterMemoryAMP, self).__init__()
+        self.momentum = momentum
+        self.temp = temp
+        self.use_hard = use_hard
+        self.features = None
+
+    def forward(self, inputs, targets, cams=None, epoch=None):
+        inputs = F.normalize(inputs, dim=1).cuda()
+        if self.use_hard:
+            outputs = cm_hard(inputs, targets, self.features, self.momentum)
+        else:
+            outputs = cm(inputs, targets, self.features, self.momentum)
+
+        outputs /= self.temp
+        loss = F.cross_entropy(outputs, targets)
+        return loss
+    
+    
+class CrossEntropyLabelSmooth(nn.Module):
+    """Cross entropy loss with label smoothing regularizer.
+
+    Reference:
+    Szegedy et al. Rethinking the Inception Architecture for Computer Vision. CVPR 2016.
+    Equation: y = (1 - epsilon) * y + epsilon / K.
+
+    Args:
+        num_classes (int): number of classes.
+        epsilon (float): weight.
+    """
+
+    def __init__(self, num_classes, epsilon=0.1, use_gpu=True):
+        super(CrossEntropyLabelSmooth, self).__init__()
+        self.num_classes = num_classes
+        self.epsilon = epsilon
+        self.use_gpu = use_gpu
+        self.logsoftmax = nn.LogSoftmax(dim=1)
+
+    def forward(self, inputs, targets):
+        """
+        Args:
+            inputs: prediction matrix (before softmax) with shape (batch_size, num_classes)
+            targets: ground truth labels with shape (num_classes)
+        """
+        log_probs = self.logsoftmax(inputs) 
+        targets = torch.zeros(log_probs.size()).scatter_(1, targets.unsqueeze(1).data.cpu(), 1) 
+        if self.use_gpu: targets = targets.cuda()
+        targets = (1 - self.epsilon) * targets + self.epsilon / self.num_classes
+        loss = (- targets * log_probs).mean(0).sum()
+        return loss
+
+
+from turtle import pd
+import torch
+from torch import nn
+
+def normalize(x, axis=-1):
+    """Normalizing to unit length along the specified dimension.
+    Args:
+      x: pytorch Variable
+    Returns:
+      x: pytorch Variable, same shape as input
+    """
+    x = 1. * x / (torch.norm(x, 2, axis, keepdim=True).expand_as(x) + 1e-12)
+    return x
+
+
+def euclidean_dist(x, y):
+    """
+    Args:
+      x: pytorch Variable, with shape [m, d]
+      y: pytorch Variable, with shape [n, d]
+    Returns:
+      dist: pytorch Variable, with shape [m, n]
+    """
+    m, n = x.size(0), y.size(0)
+    xx = torch.pow(x, 2).sum(1, keepdim=True).expand(m, n) #B, B
+    yy = torch.pow(y, 2).sum(1, keepdim=True).expand(n, m).t()
+    dist = xx + yy
+    dist = dist - 2 * torch.matmul(x, y.t())
+    # dist.addmm_(1, -2, x, y.t())
+    dist = dist.clamp(min=1e-12).sqrt()  # for numerical stability
+    return dist
+
+
+def cosine_dist(x, y):
+    """
+    Args:
+      x: pytorch Variable, with shape [m, d]
+      y: pytorch Variable, with shape [n, d]
+    Returns:
+      dist: pytorch Variable, with shape [m, n]
+    """
+    m, n = x.size(0), y.size(0)
+    x_norm = torch.pow(x, 2).sum(1, keepdim=True).sqrt().expand(m, n)
+    y_norm = torch.pow(y, 2).sum(1, keepdim=True).sqrt().expand(n, m).t()
+    xy_intersection = torch.mm(x, y.t())
+    dist = xy_intersection/(x_norm * y_norm)
+    dist = (1. - dist) / 2
+    return dist
+
+
+def hard_example_mining(dist_mat, labels, return_inds=False):
+    """For each anchor, find the hardest positive and negative sample.
+    Args:
+      dist_mat: pytorch Variable, pair wise distance between samples, shape [N, N]
+      labels: pytorch LongTensor, with shape [N]
+      return_inds: whether to return the indices. Save time if `False`(?)
+    Returns:
+      dist_ap: pytorch Variable, distance(anchor, positive); shape [N]
+      dist_an: pytorch Variable, distance(anchor, negative); shape [N]
+      p_inds: pytorch LongTensor, with shape [N];
+        indices of selected hard positive samples; 0 <= p_inds[i] <= N - 1
+      n_inds: pytorch LongTensor, with shape [N];
+        indices of selected hard negative samples; 0 <= n_inds[i] <= N - 1
+    NOTE: Only consider the case in which all labels have same num of samples,
+      thus we can cope with all anchors in parallel.
+    """
+    assert len(dist_mat.size()) == 2
+    assert dist_mat.size(0) == dist_mat.size(1)
+    N = dist_mat.size(0)
+
+    # shape [N, N]
+    is_pos = labels.expand(N, N).eq(labels.expand(N, N).t())
+    is_neg = labels.expand(N, N).ne(labels.expand(N, N).t())
+
+    # `dist_ap` means distance(anchor, positive)
+    # both `dist_ap` and `relative_p_inds` with shape [N, 1]
+    dist_ap, relative_p_inds = torch.max(
+        dist_mat[is_pos].contiguous().view(N, -1), 1, keepdim=True)
+    # print(dist_mat[is_pos].shape)
+    # `dist_an` means distance(anchor, negative)
+    # both `dist_an` and `relative_n_inds` with shape [N, 1]
+    dist_an, relative_n_inds = torch.min(
+        dist_mat[is_neg].contiguous().view(N, -1), 1, keepdim=True)
+    # shape [N]
+    dist_ap = dist_ap.squeeze(1)
+    dist_an = dist_an.squeeze(1)
+
+    if return_inds:
+        # shape [N, N]
+        ind = (labels.new().resize_as_(labels)
+               .copy_(torch.arange(0, N).long())
+               .unsqueeze(0).expand(N, N))
+        # shape [N, 1]
+        p_inds = torch.gather(
+            ind[is_pos].contiguous().view(N, -1), 1, relative_p_inds.data)
+        n_inds = torch.gather(
+            ind[is_neg].contiguous().view(N, -1), 1, relative_n_inds.data)
+        # shape [N]
+        p_inds = p_inds.squeeze(1)
+        n_inds = n_inds.squeeze(1)
+        return dist_ap, dist_an, p_inds, n_inds
+
+    return dist_ap, dist_an
+
+
+class TripletLoss(object):
+    """
+    Triplet loss using HARDER example mining,
+    modified based on original triplet loss using hard example mining
+    """
+
+    def __init__(self, margin=None, hard_factor=0.0):
+        self.margin = margin
+        self.hard_factor = hard_factor
+        if margin is not None:
+            self.ranking_loss = nn.MarginRankingLoss(margin=margin)
+        else:
+            self.ranking_loss = nn.SoftMarginLoss()
+
+    def __call__(self, global_feat, labels, normalize_feature=False):
+        if normalize_feature:
+            global_feat = normalize(global_feat, axis=-1)
+        dist_mat = euclidean_dist(global_feat, global_feat) #B,B
+        dist_ap, dist_an = hard_example_mining(dist_mat, labels)
+
+        dist_ap *= (1.0 + self.hard_factor)
+        dist_an *= (1.0 - self.hard_factor)
+
+        y = dist_an.new().resize_as_(dist_an).fill_(1)
+        if self.margin is not None:
+            loss = self.ranking_loss(dist_an, dist_ap, y)
+        else:
+            loss = self.ranking_loss(dist_an - dist_ap, y)
+        return loss
