22270391fe88c3860a012a91e3f1f9fb1e1939cc
Chenyang Yu <asuradayuci@gmail.com>
2025-07-08 16:56:22 +0800

    Update loss.py

--- climb/loss.py
+++ climb/loss.py
@@ -74,6 +74,68 @@ class CM_Hard(autograd.Function):
 
         return grad_inputs, None, None, None
 
+
+class CM_Mix_mean_hard(autograd.Function):
+
+    @staticmethod
+    def forward(ctx, inputs, indexes, features, momentum):
+        ctx.features = features
+        ctx.momentum = momentum
+        ctx.save_for_backward(inputs, indexes)
+        outputs = inputs.mm(ctx.features.t())
+
+        return outputs
+
+    @staticmethod
+    def backward(ctx, grad_outputs):
+        inputs, indexes = ctx.saved_tensors
+        nums = len(ctx.features)//2
+        grad_inputs = None
+        if ctx.needs_input_grad[0]:
+            grad_inputs = grad_outputs.mm(ctx.features)
+        for x, y in zip(inputs, indexes):
+            ctx.features[y] = ctx.momentum * ctx.features[y] + (1. - ctx.momentum) * x
+            ctx.features[y] /= ctx.features[y].norm()
+            
+        batch_centers = collections.defaultdict(list)
+        for instance_feature, index in zip(inputs, indexes.tolist()):
+            batch_centers[index].append(instance_feature)  # 找到这个ID对应的所有实例特征
+
+        ##### Mean
+        # for index, features in batch_centers.items():
+        #     feats = torch.stack(features, dim=0)
+        #     features_mean = feats.mean(0)
+        #     ctx.features[index] = ctx.features[index] * ctx.momentum + (1 - ctx.momentum) * features_mean
+        #     ctx.features[index] /= ctx.features[index].norm()
+        ##### Hard
+        for index, features in batch_centers.items():
+            distances = []
+            for feature in features:  # 计算每个实例与质心之间的距离
+                distance = feature.unsqueeze(0).mm(ctx.features[index].unsqueeze(0).t())[0][0]
+                distances.append(distance.cpu().numpy())
+
+            mean = torch.stack(features, dim=0).mean(0)  # 均值
+            ctx.features[index] = ctx.features[index] * ctx.momentum + (1 - ctx.momentum) * mean
+            ctx.features[index] /= ctx.features[index].norm()
+            
+            hard = np.argmin(np.array(distances))  #  余弦距离最近的，最不相似的  
+            ctx.features[index+nums] = ctx.features[index+nums] * ctx.momentum + (1 - ctx.momentum) * features[hard]
+            ctx.features[index+nums] /= ctx.features[index+nums].norm()
+
+            # rand = random.choice(features)  # 随机选一个
+        #### rand
+#         for index, features in batch_centers.items():
+
+#             features_mean = random.choice(features)
+
+#             ctx.features[index] = ctx.features[index] * ctx.momentum + (1 - ctx.momentum) * features_mean
+#             ctx.features[index] /= ctx.features[index].norm()
+
+        return grad_inputs, None, None, None
+
+def cm_mix(inputs, indexes, features, momentum=0.5):
+    return CM_Mix_mean_hard.apply(inputs, indexes, features, torch.Tensor([momentum]).to(inputs.device))
+
 def cm(inputs, indexes, features, momentum=0.5):
     return CM.apply(inputs, indexes, features, torch.Tensor([momentum]).to(inputs.device))
 
@@ -91,13 +153,16 @@ class ClusterMemoryAMP(nn.Module, ABC):
 
     def forward(self, inputs, targets, cams=None, epoch=None):
         inputs = F.normalize(inputs, dim=1).cuda()
-        if self.use_hard:
-            outputs = cm_hard(inputs, targets, self.features, self.momentum)
-        else:
-            outputs = cm(inputs, targets, self.features, self.momentum)
+        # if self.use_hard:
+        #     outputs = cm_hard(inputs, targets, self.features, self.momentum)
+        # else:
+        #     outputs = cm(inputs, targets, self.features, self.momentum)
 
+        outputs = cm_mix(inputs, targets, self.features, self.momentum)
         outputs /= self.temp
-        loss = F.cross_entropy(outputs, targets)
+        
+        mean, hard = torch.chunk(outputs, 2, dim=1)
+        loss = 0.5 * (F.cross_entropy(hard, targets) + F.cross_entropy(mean, targets))
         return loss
     
     
